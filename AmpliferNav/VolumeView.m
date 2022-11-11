377369a33655877ffe7e667683a2c7f377d1d15a
LinglongYan <linglongyan@icloud.com>
2022-11-11 22:03:58 +0800

    音量页面添加完成

--- AmpliferNav/VolumeView.m
+++ AmpliferNav/VolumeView.m
@@ -20,6 +20,9 @@
 @property (nonatomic) UIButton* rightChannButton;
 @property (nonatomic) UIButton* allChanButton;
 
+@property (nonatomic) UILabel* leftChannLabel;
+@property (nonatomic) UILabel* rightChannLabel;
+
 @end
 
 @implementation VolumeView
@@ -64,9 +67,68 @@
         [self addSubview:self.leftVolumeLabel];
         [self addSubview:self.rightVolumeLabel];
         
+        NSUInteger channHorizontalMargin = 65;
+        NSUInteger channTopMargin = 20;
+        NSUInteger channButtonHeight = 35;
+        NSUInteger channButtonPosY = topMargin + sliderTopMargin + sliderHeight + channTopMargin;
+        
+        UIView* channButtonView = [self createChannButtonView];
+        channButtonView.frame = CGRectMake(channHorizontalMargin, channButtonPosY, mainFrame.size.width - 2*channHorizontalMargin, channButtonHeight);
+        
+        NSUInteger labelsViewHorizontalMargin = 80;
+        NSUInteger labelsViewTopMargin = 2;
+        NSUInteger labelsViewHeight = 35;
+        NSUInteger labelsViewPosY = channButtonPosY + channButtonHeight + labelsViewTopMargin;
+        
+        UIView* labelsView = [self createLabelsView];
+        labelsView.frame = CGRectMake(labelsViewHorizontalMargin, labelsViewPosY, mainFrame.size.width - 2*labelsViewHorizontalMargin, labelsViewHeight);
+        
+        [self addSubview:channButtonView];
+        [self addSubview:labelsView];
+        
         self.hidden = true;
     }
     return self;
+}
+
+- (UIView*) createChannButtonView
+{
+    UIStackView* stackView = [[UIStackView alloc] init];
+    stackView.axis = UILayoutConstraintAxisHorizontal;
+    stackView.distribution = UIStackViewDistributionFillEqually;
+    
+    self.leftChannButton = [[UIButton alloc] init];
+    [self.leftChannButton setImage:[UIImage imageNamed:@"左"] forState:UIControlStateNormal];
+    
+    self.rightChannButton = [[UIButton alloc] init];
+    [self.rightChannButton setImage:[UIImage imageNamed:@"右"] forState:UIControlStateNormal];
+    
+    self.allChanButton = [[UIButton alloc] init];
+    [self.allChanButton setImage:[UIImage imageNamed:@"链接选中"] forState:UIControlStateNormal];
+    
+    [stackView addArrangedSubview:self.leftChannButton];
+    [stackView addArrangedSubview:self.allChanButton];
+    [stackView addArrangedSubview:self.rightChannButton];
+    
+    return (UIView*)stackView;
+}
+
+- (UIView*) createLabelsView
+{
+    UIStackView* stackView = [[UIStackView alloc] init];
+    stackView.axis = UILayoutConstraintAxisHorizontal;
+    stackView.distribution = UIStackViewDistributionEqualCentering;
+    
+    self.leftChannLabel = [[UILabel alloc] init];
+    self.rightChannLabel = [[UILabel alloc] init];
+    
+    self.leftChannLabel.text = @"左耳";
+    self.rightChannLabel.text = @"右耳";
+    
+    [stackView addArrangedSubview:self.leftChannLabel];
+    [stackView addArrangedSubview:self.rightChannLabel];
+    
+    return stackView;
 }
 
 @end
