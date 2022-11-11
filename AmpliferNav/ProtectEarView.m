8841c4c856439201407694d9246953c580451b05
LinglongYan <linglongyan@icloud.com>
2022-11-11 13:47:07 +0800

    add descption label to protect view

--- AmpliferNav/ProtectEarView.m
+++ AmpliferNav/ProtectEarView.m
@@ -19,6 +19,8 @@
 @property (nonatomic) UILabel* leftChannLabel;
 @property (nonatomic) UILabel* rightChannLabel;
 
+@property (nonatomic) UILabel* descriptionLabel;
+
 @end
 
 @implementation ProtectEarView
@@ -32,11 +34,17 @@
         self.topMargin = 20;
         self.horizontalMargin = 20;
         
+        CGRect mainFrame = [UIScreen mainScreen].bounds;
+        
         self.leftChannLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.horizontalMargin, self.topMargin, labelWidth, labelHeight)];
         self.leftChannLabel.text = @"左耳";
         
         self.rightChannLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.horizontalMargin, self.topMargin+80, labelWidth, labelHeight)];
         self.rightChannLabel.text = @"右耳";
+        
+        self.descriptionLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.horizontalMargin, self.topMargin+200, mainFrame.size.width - self.horizontalMargin*2, labelHeight*2)];
+        self.descriptionLabel.text = @"护耳模式能够压缩响度过大声音的增益。\n例如：汽车鸣笛声、爆竹声等，防止给您带来不适。";
+        self.descriptionLabel.numberOfLines = 2;
         
         self.leftSlider = [[ProtectEarSlider alloc] initWithFrame:CGRectMake(self.horizontalMargin+labelWidth+10, self.topMargin, 200, 40)];
         self.rightSlider = [[ProtectEarSlider alloc] initWithFrame:CGRectMake(self.horizontalMargin+labelWidth+10, self.topMargin+80, 200, 40)];
@@ -45,6 +53,7 @@
         [self addSubview:self.rightSlider];
         [self addSubview:self.leftChannLabel];
         [self addSubview:self.rightChannLabel];
+        [self addSubview:self.descriptionLabel];
         
         self.hidden = true;
     }
